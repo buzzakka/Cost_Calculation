bf06e2555ff4bad644bdb27dba50b7761e3e5371
Marsel <buzzakka6@gmail.com>
2024-01-24 12:11:06 +0300

    logout view added

--- users/urls.py
+++ users/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = 'users'
 
 urlpatterns = [
-    path('login/', views.UserLoginView.as_view(), name='login')
+    path('login/', views.UserLoginView.as_view(), name='login'),
+    path('logout/', views.logout_user, name='logout'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,7 +1,16 @@
 from django.contrib.auth.views import LoginView
+from django.contrib.auth import logout
+from django.shortcuts import HttpResponseRedirect
+from django.urls import reverse_lazy
 from .forms import *
 
 
 class UserLoginView(LoginView):
     template_name = 'users/login.html'
     extra_context = {'title': 'Авторизация'}
+    redirect_authenticated_user = True
+
+
+def logout_user(request):
+    logout(request)
+    return HttpResponseRedirect(reverse_lazy('users:login'))
