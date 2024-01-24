62828746cebc55e77704a4e16072387910aafd66
Marsel <buzzakka6@gmail.com>
2024-01-24 18:19:51 +0300

    Добавлена форма изменения пароля

--- users/urls.py
+++ users/urls.py
@@ -1,4 +1,5 @@
-from django.contrib.auth.views import PasswordResetDoneView, PasswordResetConfirmView, PasswordResetCompleteView
+from django.contrib.auth.views import (PasswordResetDoneView, PasswordResetConfirmView, PasswordResetCompleteView,
+                                       PasswordChangeView, PasswordResetView)
 from django.urls import path
 from django.views.generic import TemplateView
 
@@ -7,13 +8,25 @@ from .views import *
 app_name = 'users'
 
 urlpatterns = [
-    path('login/', UserLoginView.as_view(), name='login'),
+    path('login/',
+         UserLoginView.as_view(),
+         name='login'
+         ),
     path('logout/', logout_user, name='logout'),
 
     path('register/', UserRegisterView.as_view(), name='register'),
 
+    path('password-change/',
+         PasswordChangeView.as_view(
+             template_name='users/change-password.html',
+             extra_context={'title': "Изменить пароль"},
+             success_url=reverse_lazy('users:index')
+         ),
+         name='change_password'
+    ),
+
     path('password-reset/',
-         CustomPasswordResetView.as_view(
+         PasswordResetView.as_view(
              template_name='users/password_reset_form.html',
              email_template_name='users/password_reset_email.html',
              success_url=reverse_lazy('users:password_reset_done')
