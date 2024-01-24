ab632a7a3a61d35bc6d514d5376984dd82923868
Marsel <buzzakka6@gmail.com>
2024-01-24 21:48:15 +0300

    Вью из urls.py перенесены в views.py; Создан миксин для неавторизованных пользователей

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,4 @@
-from django.contrib.auth.views import (PasswordResetDoneView, PasswordResetConfirmView, PasswordResetCompleteView,
-                                       PasswordChangeView, PasswordResetView)
+from django.contrib.auth.views import PasswordResetConfirmView
 from django.urls import path
 from django.views.generic import TemplateView
 
@@ -8,42 +7,22 @@ from .views import *
 app_name = 'users'
 
 urlpatterns = [
-    path('login/',
-         UserLoginView.as_view(),
-         name='login'
-         ),
+    path('login/', UserLoginView.as_view(), name='login'),
     path('logout/', logout_user, name='logout'),
 
     path('register/', UserRegisterView.as_view(), name='register'),
 
-    path('password-change/',
-         PasswordChangeView.as_view(
-             template_name='users/change-password.html',
-             extra_context={'title': "Изменить пароль"},
-             success_url=reverse_lazy('users:index')
-         ),
-         name='change_password'
-    ),
+    path('change-password/', UserChangePasswordView.as_view(), name='change_password'),
 
-    path('password-reset/',
-         PasswordResetView.as_view(
-             template_name='users/password_reset_form.html',
-             email_template_name='users/password_reset_email.html',
-             success_url=reverse_lazy('users:password_reset_done')
-         ),
-         name='password_reset'),
-    path('password-reset/done/',
-         PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'),
-         name='password_reset_done'),
+    path('password-reset/', UserPasswordResetView.as_view(), name='password_reset'),
+    path('password-reset/done/', UserPasswordResetDoneView.as_view(), name='password_reset_done'),
     path('password-reset/<uidb64>/<token>/',
          PasswordResetConfirmView.as_view(
              template_name='users/password_reset_confirm.html',
              success_url=reverse_lazy('users:password_reset_complete')
          ),
          name='password_reset_confirm'),
-    path('password-reset/complete/',
-         PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'),
-         name='password_reset_complete'),
+    path('password-reset/complete/', UserPasswordResetCompleteView.as_view(), name='password_reset_complete'),
 
     path('index/', TemplateView.as_view(template_name='index.html'), name='index')
 ]
--- users/views.py
+++ users/views.py
@@ -1,10 +1,20 @@
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import (PasswordResetCompleteView, PasswordChangeView, PasswordResetView, LoginView,
+                                       PasswordResetDoneView)
 from django.contrib.auth import logout
 from django.shortcuts import HttpResponseRedirect
 from django.urls import reverse_lazy
+from django.views import View
 from django.views.generic import CreateView
 
 from .forms import *
+
+
+class UnauthorizedOnlyMixin(View):
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return HttpResponseRedirect(reverse_lazy('users:index'))
+        return super().dispatch(request, *args, **kwargs)
 
 
 class UserLoginView(LoginView):
@@ -12,15 +22,42 @@ class UserLoginView(LoginView):
     extra_context = {'title': 'Авторизация'}
     redirect_authenticated_user = True
 
+    def get_success_url(self) -> str:
+        return reverse_lazy('users:index')
+
 
 def logout_user(request):
     logout(request)
     return HttpResponseRedirect(reverse_lazy('users:login'))
 
 
-class UserRegisterView(CreateView):
+class UserRegisterView(UnauthorizedOnlyMixin, CreateView):
     template_name = 'users/register.html'
     form_class = UserRegisterForm
     extra_context = {'title': 'Регистрация'}
+
+    def get_success_url(self) -> str:
+        return reverse_lazy('users:index')
+
+
+class UserChangePasswordView(LoginRequiredMixin, PasswordChangeView):
+    template_name = 'users/change_password.html'
+    extra_context = {'title': 'Изменить пароль'}
     success_url = reverse_lazy('users:index')
 
+
+class UserPasswordResetView(UnauthorizedOnlyMixin, PasswordResetView):
+    template_name = 'users/password_reset_form.html'
+    extra_context = {'title': 'Сброс пароля'}
+    email_template_name = 'users/password_reset_email.html'
+    success_url = reverse_lazy('users:password_reset_done')
+
+
+class UserPasswordResetCompleteView(UnauthorizedOnlyMixin, PasswordResetCompleteView):
+    template_name = 'users/password_reset_complete.html'
+    extra_context = {'title': 'Сброс пароля'}
+
+
+class UserPasswordResetDoneView(UnauthorizedOnlyMixin, PasswordResetDoneView):
+    template_name = 'users/password_reset_done.html'
+    extra_context = {'title': 'Сброс пароля'}
