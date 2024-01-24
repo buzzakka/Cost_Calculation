2e95d3dd729d0bc55f3c29737113577e99a79ec5
Marsel <buzzakka6@gmail.com>
2024-01-24 14:02:53 +0300

    Добавлено кастомное отображение модели пользователя в админке

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import CustomUser
 
-# Register your models here.
+
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ('email', 'username', 'date_joined')
+    fields = ('email', 'username', 'date_joined', 'image', 'groups')
+    search_fields = ('email', 'username')
