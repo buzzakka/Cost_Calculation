5bff73f25d328de1bc2a74161b25697c156f7006
Marsel <buzzakka6@gmail.com>
2024-01-26 10:16:45 +0300

    график перенесен в main.html

--- costs/urls.py
+++ costs/urls.py
@@ -6,5 +6,4 @@ app_name = 'costs'
 
 urlpatterns = [
     path('main', MainView.as_view(), name='main'),
-    path('graph', GraphView.as_view(), name='graph'),
 ]
--- costs/views.py
+++ costs/views.py
@@ -11,14 +11,8 @@ class MainView(LoginRequiredMixin, TemplateView):
     template_name = 'index.html'
     login_url = reverse_lazy('users:login')
 
-
-class GraphView(LoginRequiredMixin, TemplateView):
-    template_name = 'graph.html'
-
     def get(self, request, *args, **kwargs):
         user = request.user
         graph_info = Cost.objects.filter(user=user).values('category__name').annotate(Sum('value'))
         chart_data = [{'name': elem['category__name'], 'y': float(elem['value__sum'])} for elem in graph_info]
-        print(chart_data)
-        return render(request, 'costs/graph.html', {'chart_data': chart_data})
-
+        return render(request, 'costs/main.html', {'chart_data': chart_data})
