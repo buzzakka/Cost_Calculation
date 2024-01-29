b25f4dace4e37bc10a086fbeb573415f9f8c0a90
Marsel <buzzakka6@gmail.com>
2024-01-29 17:54:32 +0300

    убраны лишние импорты"

--- api_v1/views.py
+++ api_v1/views.py
@@ -1,7 +1,6 @@
-from rest_framework import generics, status
+from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.response import Response
 from django_filters.rest_framework import DjangoFilterBackend
 from .serializer import *
 from .filters import *
