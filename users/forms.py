a36bb79d8a175a3617fbf2012e6b93ed39bce0a7
Marsel <buzzakka6@gmail.com>
2024-02-07 14:27:39 +0300

    Изменены стили форм

--- users/forms.py
+++ users/forms.py
@@ -4,6 +4,13 @@ from django.contrib.auth.forms import UserCreationForm
 
 class UserRegisterForm(UserCreationForm):
     """ Форма регистрации пользователя """
+
+    def __init__(self, *args, **kwargs):
+        super(UserRegisterForm, self).__init__(*args, **kwargs)
+
+        for fieldname in ('username', 'email', 'password1', 'password2'):
+            self.fields[fieldname].help_text = None
+
     class Meta:
         model = get_user_model()
         fields = ('username', 'email', 'password1', 'password2')
